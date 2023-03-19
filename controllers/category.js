75bfa87ab9b2fe97e57c4989c83eb9428953dc21
teklu <teklufa@gmail.com>
2023-03-19 11:52:47 +0300

    return list of categories

--- controllers/category.js
+++ controllers/category.js
@@ -1,7 +1,8 @@
-const index = (req, res) => {
-  res.status(200).json({
-    message: 'categories'
-  })
+import db from '../config/db.js'
+
+const index = async (req, res) => {
+  const categories = await db.category.findMany({})
+  res.status(200).json(categories)
 }
 
 export default {
